5693cbf3f0fbf5171742ea2ac226f83805e44a16
David Akang <akangdavd68@yahoo.com>
2023-11-28 08:45:30 +0000

    Remove singleQuote option from .prettierrc and add console.log statements in script.js

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,37 @@
 'use strict';
+
+console.log(this); // window
+
+//Regular Function
+const calcAge = function (birthYear) {
+  console.log(2021 - birthYear);
+  console.log(this); // undefined, when not in strict mode this points to window
+}
+
+calcAge(1991);  // 30
+
+//Arrow Function
+const calcAgeArrow =  (birthYear) => {
+    console.log(2021 - birthYear);
+    console.log(this); 
+  }
+
+  calcAgeArrow(1980)
+
+const David = {
+    year: 1999,
+    calcAge: function () {
+        console.log(this); // David Object
+        console.log(2023 - this.year);
+    }
+}
+
+David.calcAge()
+
+const matilda = {
+    year: 2017,
+
+};
+
+matilda.calcAge = David.calcAge;
+matilda.calcAge()
